00fa294013cc29fe7a6ca12b168697b71cff588a
AlejoRetola <116813802+AlejoRetola@users.noreply.github.com>
2024-09-20 10:06:25 -0300

    Add files via upload

--- ej4/src/main/java/ejercicio4/ej4/Balanza.java
+++ ej4/src/main/java/ejercicio4/ej4/Balanza.java
@@ -3,29 +3,40 @@ package ejercicio4.ej4;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Balanza extends ClasePadre {
+public class Balanza {
     private List<Producto> listaP;
 
     public Balanza() {
-        super(0, 0, 0);
+       // super(0, 0, 0);
         this.listaP = new ArrayList<Producto>();
     }
 
     public void ponerEnCero() {
-        this.cantidadDeProductos = 0;
-        this.precioTotal = 0;
-        this.pesoTotal = 0;
-        this.listaP.clear();
+        this.listaP = new ArrayList<Producto> () ;
     }
 
     public void agregarProducto(Producto unProducto) {
-        this.cantidadDeProductos++;
-        this.precioTotal += unProducto.getPrecio();
-        this.pesoTotal += unProducto.getPeso();
         this.listaP.add(unProducto);
     }
 
+    public int getCantidadDeProductos() {
+    	return (int) this.listaP.stream().count();
+    }
+    
+    public double getPesoTotal() {
+    	return this.listaP.stream().mapToDouble(producto -> producto.getPeso()).sum();
+    }
+    
+    public double getPrecioTotal() {
+    	return this.listaP.stream().mapToDouble(producto -> producto.getPrecio()).sum();
+    }
+    
+    public List<Producto> getListaProductos() {
+    	List<Producto> copia = this.listaP; // USAR ARRAYS.UTILS
+    	return copia;
+    }
+    
     public Ticket emitirTicket() {
-        return new Ticket(this.cantidadDeProductos, this.pesoTotal, this.precioTotal, this.listaP);
+        return new Ticket(this.getCantidadDeProductos(), this.getPesoTotal(), this.getPrecioTotal(), this.listaP);
     }
 }
--- ej4/src/main/java/ejercicio4/ej4/Ticket.java
+++ ej4/src/main/java/ejercicio4/ej4/Ticket.java
@@ -3,12 +3,17 @@ package ejercicio4.ej4;
 import java.time.LocalDate;
 import java.util.List;
 
-public class Ticket extends ClasePadre {
+public class Ticket{
     private LocalDate fecha;
     private List<Producto> productos;
+    protected int cantidadDeProductos;
+    protected double pesoTotal;
+    protected double precioTotal;
 
     public Ticket(int cantidad, double peso, double precio, List<Producto> productos) {
-        super(cantidad, peso, precio);
+        this.cantidadDeProductos = cantidad;
+        this.pesoTotal = peso;
+        this.precioTotal = precio;
         this.fecha = LocalDate.now();
         this.productos = productos;
     }
@@ -24,4 +29,18 @@ public class Ticket extends ClasePadre {
     public List<Producto> getProductos() {
         return this.productos;
     }
+    
+
+    public int getCantidadDeProductos() {
+        return this.cantidadDeProductos;
+    }
+
+    public double getPesoTotal() {
+        return this.pesoTotal;
+    }
+
+    public double getPrecioTotal() {
+        return this.precioTotal;
+    }
+    
 }
